9d9bc03729b852ed7c33e628f3d3bd7befb32d57
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-11-11 00:43:46 +0800

    import files from theme

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -1,4 +1,26 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./assets/sass/app.css";
+// import "./assets/css/demo.css";
+// import "./assets/css/pe-icon-7-stroke.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
+
+import indexRoutes from "./routes/index.jsx";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/sass/app.css";
+import "./assets/sass/app";
 import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
+
+ReactDOM.render(
+  <HashRouter>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} component={prop.component} key={key} />;
+      })}
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
+);
